be22e316e0c867c71a9ff2c19153c9b59b0e7ab1
joaooujdq <joaooujdqsa@outlook.com>
2020-07-08 13:25:01 -0300

    adicionando elementos do body da pagina

--- web/src/pages/Shop/index.tsx
+++ web/src/pages/Shop/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './styles.css';
 import plant from '../../assets/plant.svg';
-import { FiSearch } from 'react-icons/fi';
+import { FiSearch, FiShoppingCart, FiHeart, FiUser, FiChevronDown } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 
 const Shop: React.FC = () => {
@@ -14,25 +14,57 @@ const Shop: React.FC = () => {
             <div id="reference">
                <img src={plant} alt="Logo"/>
             <div >
-            Icons made by <a href="https://www.flaticon.com/free-icon/green-tea_1087420" 
+            Icons made by <a target="_blank" href="https://www.flaticon.com/free-icon/green-tea_1087420" 
             title="dDara">dDara</a> from 
-            <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+            <a target="_blank" href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
             </div>
             </div>
-            
-            
             <input type="text"/>
-            <Link to="../page-home">
+            <Link to="/" id="searchButton">
             <span>
-                <FiSearch/>
+                <FiSearch id="fiSearch"/>
             </span>
             </Link>
+            <Link to="/" id="userButtons">
+                <FiShoppingCart id="userIcons"/>
+            </Link>
+            <Link to="/" id="userButtons">
+                <FiHeart id="userIcons"/>
+            </Link>
+            <Link to="/" id="userButtons">
+                <FiUser id="userIcons"/>
+            </Link>
             </div>
-            
-            
          </header>
          <body>
-         
+        <div id="productsMenuBar">
+
+            <button className="productsMenuButton" id="npButton">
+             Natural Plants</button>
+         <button className="productsMenuButton" id="apbutton">
+             Artificial Plants</button>
+         <button className="productsMenuButton" id="seedsButton">
+             Seeds</button>
+         <button className="productsMenuButton" id="acessoriesButton">
+             Acessories</button>
+         </div>
+         <h2>Natural Plants</h2>
+         <h3> <span className="tab"/> Natural plants and flowers can bring life 
+         to your home environments. They can help to create points of color, not
+         to mention that they can scent the places. Here at the Plant Shop, we 
+         have flowers in different sizes for you. Some species are great for 
+         indoor environments and can survive very well only with indirect light. 
+         The most recommended species are the water stick, singon, pleomele, 
+         anthurium, aloe vera, peace lily, begonia, zamioculca and stone rose.
+         </h3>
+         <div id="productsContainer">
+             <div id="sortingBar">
+                 <h4>Sort by</h4>
+                 <button>Best selling 
+                     <FiChevronDown id="FiChevronDown"/>
+                 </button>
+             </div>
+         </div>
          </body>
          </div>
      </div>
